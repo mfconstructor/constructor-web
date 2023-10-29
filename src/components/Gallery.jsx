c2910b471ca8da298209f1e3eeb2fff714e89b9b
mfconstructor <wojciechowskimichal664@gmail.com>
2023-10-29 19:12:09 +0100

    add images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,65 +1,15 @@
 import React from "react";
 import styles from "../style";
 import ImageGallery from "react-image-gallery";
-import { photo2, photo3 } from "../assets";
+import { imageUrls } from "../constants";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const Gallery = () => {
-  const images = [
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-    {
-      original: photo2,
-      thumbnail: photo2,
-    },
-    {
-      original: photo3,
-      thumbnail: photo3,
-    },
-  ];
   return (
     <section id="realizacje" className={`${styles.paddingY} flex-col`}>
       <h2 className={styles.heading2}>Realizacje</h2>
       <ImageGallery
-        items={images}
+        items={imageUrls}
         showPlayButton={false}
         lazyLoad={true}
         slideDuration={200}
